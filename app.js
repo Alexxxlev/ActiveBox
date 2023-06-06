dc0afa2c667ed5efef50b645b43eeb5c6b6fa8df
Alexander <102146514+Alexxxlev@users.noreply.github.com>
2023-06-06 11:34:05 +0300

    scripts

--- app.js
+++ app.js
@@ -1,76 +1,77 @@
-$(function() {
+$(function () {
+  /* Fixed Header */
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
 
+  checkScroll(scrollPos, introH);
 
-    /* Fixed Header */ 
-    let header = $("#header");
-    let intro = $("#intro");
-    let introH = intro.innerHeight();
+  $(window).on("scroll resize", function () {
     let scrollPos = $(window).scrollTop();
-    let nav = $("#nav");
-    let navToggle = $("#navToggle");
+
+    scrollPos = $(this).scrollTop();
 
     checkScroll(scrollPos, introH);
+  });
 
-    $(window).on("scroll resize", function() {
-        let introH = intro.innerHeight();
-        scrollPos = $(this).scrollTop();
-
-        checkScroll(scrollPos, introH);
-    });
-
-    function checkScroll(scrollPos, introH) {
-        if( scrollPos > introH ) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
+  function checkScroll(scrollPos, introH) {
+    if (scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
     }
+  }
 
+  /* Smooth scroll */
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
 
-    /* Smooth scroll */ 
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
+    let elementID = $(this).data("scroll");
+    let elementOffset = $(elementID).offset().top;
 
-        let elementID = $(this).data('scroll');
-        let elementOffset = $(elementID).offset().top;
+    nav.removeClass("show");
 
-        nav.removeClass("show");
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
 
-        $("html, bode").animate({
-            scrollTop: elementOffset -70
-        }, 700);
-    });
+  /* burger menu */
+  // обработчик на меню
+  $(".burger").on("click", function (e) {
+    $(".nav").toggleClass("nav--active");
+  });
+  // если клик вне области меню = закрыть меню
+  $(document).on("click", function (e) {
+    if (
+      $(e.target).closest(".nav").length == 0 &&
+      $(".nav").hasClass("nav--active") &&
+      $(e.target).closest(".burger").length == 0
+    ) {
+      $(".nav").toggleClass("nav--active");
+    }
+  });
+  // если клик на ссылку внутри меню = закрыть меню
+  $(".nav__link").on("click", function (e) {
+    $(".nav").toggleClass("nav--active");
+  });
 
+  /* Reviews slider:
+    https://kenwheeler.github.io/slick/ */
+  let slider = $("#reviewsSlider");
 
-    /* Nav Toggle */
-    navToggle.on("click", function(event) {
-        event.preventDefault();
-
-        nav.toggleClass("show");
-    });
-
-
-    /* Reviews: https://kenwheeler.github.io/slick/ */
-    let slider = $("#reviewsSlider");
-
-    slider.slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        dots: true
-      });
-
-
-
-
-
-
-
-
-
-
-
-
+  slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
 });
